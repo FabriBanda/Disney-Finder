57a8989f4561e21cef6e8693492bd88c31f816e4
Fabricio Banda <fabribanda438@gmail.com>
2026-03-07 13:00:50 -0600

    Initial Commit

---
+++ DisneyFinder/DisneyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyFinderApp.swift
+//  DisneyFinder
+//
+//  Created by Fabricio Banda on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DisneyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
