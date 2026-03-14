e8b3cee435cd0a9d623039f78504b36363ec3874
Fabricio Banda <fabribanda438@gmail.com>
2026-03-14 14:25:17 -0600

    handle errors

---
+++ DisneyFinder/Domain/Errors/RepositoryError.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoryError.swift
+//  DisneyFinder
+//
+//  Created by Fabricio Banda on 13/03/26.
+//
+
+import Foundation
---
+++ DisneyFinder/Presentation/UI/Screens/HomeScreen/Components/AlertError.swift
@@ -0,0 +1,18 @@
+//
+//  AlertError.swift
+//  DisneyFinder
+//
+//  Created by Fabricio Banda on 14/03/26.
+//
+
+import SwiftUI
+
+struct AlertError: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertError()
+}
